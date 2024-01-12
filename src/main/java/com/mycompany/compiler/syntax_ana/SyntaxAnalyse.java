39663c5c5ec10e6c8c868c69ac10bfb816ca9e07
hediske <mohamedhedi.aissi@yahoo.fr>
2024-01-13 00:35:53 +0100

    added first for parser

--- src/main/java/com/mycompany/compiler/syntax_ana/SyntaxAnalyse.java
+++ src/main/java/com/mycompany/compiler/syntax_ana/SyntaxAnalyse.java
@@ -1,11 +1,11 @@
 package com.mycompany.compiler.syntax_ana;
 public class SyntaxAnalyse {
     private  String[] gram = new String[]{
-        "P → D ; I",
+        "Pro → D ; I",
         "D → D ; D | id : T | id : F",
         "T → char | int | bool | string",
-        "F → function ( P ) : T | function () : T",
-        "I → I ; I | if ( E ) { I } | if ( E ) { I } ; else { I } | while ( E ) { I } | id = E | id = function (Par) { I } | id = function () { I }",
+        "F → function ( P ) : T | function ( ) : T",
+        "I → I ; I | if ( E ) { I } | if ( E ) { I } ; else { I } | while ( E ) { I } | id = E | id = function ( Par ) { I } | id = function ( ) { I }",
         "E → nb | id | str | litteral | E ( P' ) | E ( ) | E opari E | E opbol E | E oprel E | opuni E",
         // "str → \" C \"",
         // "C → litteral C | ε",
@@ -18,8 +18,15 @@ public class SyntaxAnalyse {
         // "opuni → ! | -"
 
     };
-    SLR_parser sl = new SLR_parser(gram);
 
+    private String[] NonTerminal = new String[]{
+      ";" , ":" , "char" , "bool", "string", "int" , "(" , ")" , "{", "}" , "if" , " else " , "while" , "function" , "=" , "nb" ,"id" , "str" , "litteral" , "opari" , "opbol" ,"oprel" , "opuni"
+    };
+    SLR_parser sl = new SLR_parser(gram,NonTerminal);
+    public void ShowRules(){
+        sl.ShowRules();
+        sl.GetFirst();
+    }
 }
 
     
